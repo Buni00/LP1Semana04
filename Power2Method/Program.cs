bbdc7a355a5e506af7317ce7b881c41531000716
Buni00 <bruno_hrf@outlook.com>
2023-03-22 22:24:17 +0000

    Add method

---
+++ Power2Method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Power2Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+        private static void PowersOf2Until5()
+        {
+            for (int i = 1; i <= (i << 5); i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
