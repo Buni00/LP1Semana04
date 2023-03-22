422506363c0d076a0c0b07ea90ea2e77fdd414df
Buni00 <bruno_hrf@outlook.com>
2023-03-22 22:44:27 +0000

    Call method 2 times

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -6,9 +6,12 @@ namespace Power2Method
     {
         static void Main(string[] args)
         {
-            
+            PowersOf2Until5();
+            PowersOf2Until5();
 
         }
+
+
         private static void PowersOf2Until5()
         {
             for (int i = 1; i <= (i << 5); i << 1)
