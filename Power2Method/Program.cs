1bbe15c0aa4f4722c2bbfb6ad9935dc1b16ab86a
Buni00 <bruno_hrf@outlook.com>
2023-03-23 18:05:13 +0000

    Add XML comments

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -4,22 +4,32 @@ namespace Power2Method
 {
     class Program
     {
+        /// <summary>
+        /// Prints Powers of 2 until 5 two times and then Powers of 2 until 2 and 3
+        /// </summary>
         static void Main(string[] args)
         {
-           PowersOf2Until5();
-           PowersOf2Until5();
+            PowersOf2Until5();
+            PowersOf2Until5();
 
             PowersOf2UntilN(2);
             PowersOf2UntilN(3);
 
         }
 
-
+        /// <summary>
+        /// Prints powers of 2 until 5
+        /// </summary>
         private static void PowersOf2Until5()
         {
             PowersOf2UntilN(5);
         }
 
+
+        /// <summary>
+        /// prints powers of 2 until n
+        /// </summary>
+        /// <param name="n"> last power to be printed </param>
         private static void PowersOf2UntilN (int n)
         {
             for (int i = 1; i <= (1 << n); i = i << 1)
